05a23c2aa8eb9ba4c3e047590b642e0bf3d34c78
Junhyunny <kang3966@naver.com>
2025-11-30 20:59:38 +0900

    Add chapter 05 example

---
+++ chapter_05/my-agent/agent.py
@@ -0,0 +1,14 @@
+from google.adk.agents import Agent
+
+
+def greet_user() -> str:
+  return "Hello World!"
+
+
+root_agent = Agent(
+  name="hello_agent",
+  model="gemini-2.5-flash",
+  description="유저와 인사하는 에이전트입니다.",
+  instruction="사용자에게 반갑고 친절하게 인사해주세요.",
+  tools=[greet_user],
+)
